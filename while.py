2a1f8607c4ea1f1b260bb520a01f53ff32635b74
ClaudeteNeta <claudete.neta@eq.ufrj.br>
2024-11-10 12:29:32 -0300

    Que Aula!

---
+++ while.py
@@ -0,0 +1,11 @@
+import math
+
+# Várias funções diponíveis dentro dessa biblioteca que funciona como um mode do Minecraft 
+# Cubos representam as funções e é necessário adicionar parenteses para executar, ou seja, precisam de parâmetros
+
+n = 2.718
+
+
+print(1/2**math.log(n, math.e))
+
+
